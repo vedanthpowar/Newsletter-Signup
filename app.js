af346ffe8eeb5c222214406848e58949cb09c9ac
vedanthpowar <vedanthpowar999@gmail.com>
2023-01-24 17:15:11 +0530

    dynamic port added

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post("/", function (req, res) {
 app.post("/fail",function(req,res){
     res.redirect("/");
 })
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Yeah its up and running");
 })
 
